657e188049dcc2fee474484b80b8fb99561f9518
Yusuke Morimoto <yusuke.morimoto@gmail.com>
2013-12-29 17:39:18 +0900

    retry every step

--- rec_radiko.sh
+++ rec_radiko.sh
@@ -8,6 +8,163 @@ playerurl=http://radiko.jp/player/swf/player_4.0.0.00.swf
 playerfile=./player.swf
 keyfile=./authkey.png
 
+recordingdir=/var/www/Radio
+
+#
+# get player
+#
+get_player() {
+    if [ ! -f $playerfile ]; then
+	wget -q -O $playerfile $playerurl
+
+	if [ $? -ne 0 ]; then
+	    echo "failed get player"
+	    return 1
+	fi
+    fi
+}
+
+#
+# get keydata (need swftool)
+#
+get_keydata() {
+    if [ ! -f $keyfile ]; then
+	swfextract -b 14 $playerfile -o $keyfile
+
+	if [ ! -f $keyfile ]; then
+	    echo "failed get keydata"
+	    return 1
+	fi
+    fi
+}
+
+#
+# access auth1_fms
+#
+auth1() {
+    id="${channel}_${dir}_$(date +'%Y%m%d%H%M%S')"
+
+    rm -f "auth1_fms_${id}"
+    wget -q \
+	--header="pragma: no-cache" \
+	--header="X-Radiko-App: pc_1" \
+	--header="X-Radiko-App-Version: 2.0.1" \
+	--header="X-Radiko-User: test-stream" \
+	--header="X-Radiko-Device: pc" \
+	--post-data='\r\n' \
+	--no-check-certificate \
+	--save-headers \
+	--tries=5 \
+	--timeout=5 \
+	-O "auth1_fms_${id}" \
+	https://radiko.jp/v2/api/auth1_fms
+
+    if [ $? -ne 0 ]; then
+	echo "failed auth1 process"
+	return 1
+    fi
+
+    #
+    # get partial key
+    #
+    authtoken=`cat "auth1_fms_${id}" | perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)'`
+    offset=`cat "auth1_fms_${id}" | perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)'`
+    length=`cat "auth1_fms_${id}" | perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)'`
+
+    partialkey=`dd if=$keyfile bs=1 skip=${offset} count=${length} 2> /dev/null | base64`
+
+    echo "authtoken: ${authtoken} \noffset: ${offset} length: ${length} \npartialkey: $partialkey"
+
+    rm -f "auth1_fms_${id}"
+}
+
+#
+# access auth2_fms
+#
+auth2() {
+    rm -f "auth2_fms_${id}"
+    wget -q \
+	--header="pragma: no-cache" \
+	--header="X-Radiko-App: pc_1" \
+	--header="X-Radiko-App-Version: 2.0.1" \
+	--header="X-Radiko-User: test-stream" \
+	--header="X-Radiko-Device: pc" \
+	--header="X-Radiko-Authtoken: ${authtoken}" \
+	--header="X-Radiko-Partialkey: ${partialkey}" \
+	--post-data='\r\n' \
+	--no-check-certificate \
+	--tries=5 \
+	--timeout=5 \
+	-O "auth2_fms_${id}" \
+	https://radiko.jp/v2/api/auth2_fms
+
+    if [ $? -ne 0 -o ! -f "auth2_fms_${id}" ]; then
+	echo "failed auth2 process"
+	return 1
+    fi
+
+    echo "authentication success"
+
+    areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' auth2_fms_${id}`
+    echo "areaid: $areaid"
+
+    rm -f "auth2_fms_${id}"
+}
+
+record() {
+    #
+    # rtmpdump
+    #
+    title=`date +"${name} %Y-%m-%d"`
+    basename=`date +"$recordingdir/${dir:+$dir/}${name//\//-}_${channel}_%Y-%m-%d_%H.%M.%S"`
+    flv="${basename}.flv"
+    m4a="${basename}.m4a"
+    mkdir -p "$(dirname "$basename")" # basename may contain '/'
+
+    rtmpdump -q \
+        -B $stop \
+        -r "rtmpe://w-radiko.smartstream.ne.jp" \
+        --playpath "simul-stream.stream" \
+        --app "${channel}/_definst_" \
+        -W $playerurl \
+        -C S:"" -C S:"" -C S:"" -C S:$authtoken \
+        --live \
+        --flv "$flv"
+    if [ $? -eq 1 -o `wc -c "$flv" | awk '{print $1}'` -lt 10240 ]; then
+	return 1
+    fi
+
+    ffmpeg -y -i "$flv" -vn -acodec copy \
+	-metadata title="$title" \
+	-metadata album="$album" \
+	-metadata artist="$artist" \
+	-metadata genre="Radio" \
+	"$m4a" \
+	&& rm "$flv"
+}
+
+
+with_retry() {
+    cmd=$1
+    retries=0
+    while :; do
+	$cmd
+	if [ $? -eq 0 ]; then
+	    break
+	elif [ $retries -ge 10 ]; then
+	    echo "failed $cmd"
+	    exit 1
+	else
+	    retries=$(($retries + 1))
+	    sleep $retries
+	    echo "$cmd retry: $retries"
+	fi
+    done
+}
+
+#
+# main
+#
 if [ $# -ge 4 ]; then
   channel=$1
   stop=$(($2 * 60))
@@ -20,135 +177,8 @@ else
   exit 1
 fi
 
-#
-# get player
-#
-if [ ! -f $playerfile ]; then
-  wget -q -O $playerfile $playerurl
-
-  if [ $? -ne 0 ]; then
-    echo "failed get player"
-    exit 1
-  fi
-fi
-
-#
-# get keydata (need swftool)
-#
-if [ ! -f $keyfile ]; then
-  swfextract -b 14 $playerfile -o $keyfile
-
-  if [ ! -f $keyfile ]; then
-    echo "failed get keydata"
-    exit 1
-  fi
-fi
-
-#
-# access auth1_fms
-#
-id="${channel}_${dir}_$(date +'%Y%m%d%H%M%S')"
-
-rm -f "auth1_fms_${id}"
-wget -q \
-     --header="pragma: no-cache" \
-     --header="X-Radiko-App: pc_1" \
-     --header="X-Radiko-App-Version: 2.0.1" \
-     --header="X-Radiko-User: test-stream" \
-     --header="X-Radiko-Device: pc" \
-     --post-data='\r\n' \
-     --no-check-certificate \
-     --save-headers \
-     --tries=5 \
-     --timeout=5 \
-     -O "auth1_fms_${id}" \
-     https://radiko.jp/v2/api/auth1_fms
-
-if [ $? -ne 0 ]; then
-  echo "failed auth1 process"
-  exit 1
-fi
-
-#
-# get partial key
-#
-authtoken=`cat "auth1_fms_${id}" | perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)'`
-offset=`cat "auth1_fms_${id}" | perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)'`
-length=`cat "auth1_fms_${id}" | perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)'`
-
-partialkey=`dd if=$keyfile bs=1 skip=${offset} count=${length} 2> /dev/null | base64`
-
-echo "authtoken: ${authtoken} \noffset: ${offset} length: ${length} \npartialkey: $partialkey"
-
-rm -f "auth1_fms_${id}"
-
-#
-# access auth2_fms
-#
-rm -f "auth2_fms_${id}"
-wget -q \
-     --header="pragma: no-cache" \
-     --header="X-Radiko-App: pc_1" \
-     --header="X-Radiko-App-Version: 2.0.1" \
-     --header="X-Radiko-User: test-stream" \
-     --header="X-Radiko-Device: pc" \
-     --header="X-Radiko-Authtoken: ${authtoken}" \
-     --header="X-Radiko-Partialkey: ${partialkey}" \
-     --post-data='\r\n' \
-     --no-check-certificate \
-     --tries=5 \
-     --timeout=5 \
-     -O "auth2_fms_${id}" \
-     https://radiko.jp/v2/api/auth2_fms
-
-if [ $? -ne 0 -o ! -f "auth2_fms_${id}" ]; then
-  echo "failed auth2 process"
-  exit 1
-fi
-
-echo "authentication success"
-
-areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' auth2_fms_${id}`
-echo "areaid: $areaid"
-
-rm -f "auth2_fms_${id}"
-
-#
-# rtmpdump
-#
-title=`date +"${name} %Y-%m-%d"`
-basename=`date +"/var/www/Radio/${dir:+$dir/}${name//\//-}_${channel}_%Y-%m-%d_%H.%M.%S"`
-flv="${basename}.flv"
-m4a="${basename}.m4a"
-mkdir -p "$(dirname "$basename")" # basename may contain '/'
-
-retries=0
-while :; do
-  rtmpdump -q \
-           -B $stop \
-           -r "rtmpe://w-radiko.smartstream.ne.jp" \
-           --playpath "simul-stream.stream" \
-           --app "${channel}/_definst_" \
-           -W $playerurl \
-           -C S:"" -C S:"" -C S:"" -C S:$authtoken \
-           --live \
-           --flv "$flv"
-  if [ $? -ne 1 -o `wc -c "$flv" | awk '{print $1}'` -ge 10240 ]; then
-    break
-  elif [ $retries -ge 6 ]; then
-    echo "failed rtmpdump"
-    exit 1
-  else
-    retries=$(($retries + 1))
-    sleep $(($retries * 2))
-    echo "rtmpdump retry: $retries"
-  fi
-done
-
-ffmpeg -y -i "$flv" -vn -acodec copy \
-       -metadata title="$title" \
-       -metadata album="$album" \
-       -metadata artist="$artist" \
-       -metadata genre="Radio" \
-       "$m4a" \
-  && rm "$flv"
+with_retry get_player
+with_retry get_keydata
+with_retry auth1
+with_retry auth2
+with_retry record
