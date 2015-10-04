c90afd1e8a16696a6386cda9c1a2e7b64db1f21a
Yusuke Morimoto <yusuke.morimoto@gmail.com>
2015-10-04 09:06:40 +0900

    Untabify and fix indentations. Use better words.

--- rec_radiko.sh
+++ rec_radiko.sh
@@ -16,12 +16,12 @@ recordingdir=/var/www/Radio
 #
 get_player() {
     if [ ! -f $playerfile ]; then
-	wget -q -O $playerfile $playerurl
+        wget -q -O $playerfile $playerurl
 
-	if [ $? -ne 0 ]; then
-	    echo "failed get player"
-	    return 1
-	fi
+        if [ $? -ne 0 ]; then
+            echo "get_player failed"
+            return 1
+        fi
     fi
 }
 
@@ -30,12 +30,12 @@ get_player() {
 #
 get_keydata() {
     if [ ! -f $keyfile ]; then
-	swfextract -b 14 $playerfile -o $keyfile
+        swfextract -b 14 $playerfile -o $keyfile
 
-	if [ ! -f $keyfile ]; then
-	    echo "failed get keydata"
-	    return 1
-	fi
+        if [ ! -f $keyfile ]; then
+            echo "get_keydata failed"
+            return 1
+        fi
     fi
 }
 
@@ -45,22 +45,22 @@ get_keydata() {
 auth1() {
     rm -f "auth1_fms_${pid}"
     wget -q \
-	--header="pragma: no-cache" \
-	--header="X-Radiko-App: pc_1" \
-	--header="X-Radiko-App-Version: 2.0.1" \
-	--header="X-Radiko-User: test-stream" \
-	--header="X-Radiko-Device: pc" \
-	--post-data='\r\n' \
-	--no-check-certificate \
-	--save-headers \
-	--tries=5 \
-	--timeout=5 \
-	-O "auth1_fms_${pid}" \
-	https://radiko.jp/v2/api/auth1_fms
+        --header="pragma: no-cache" \
+        --header="X-Radiko-App: pc_1" \
+        --header="X-Radiko-App-Version: 2.0.1" \
+        --header="X-Radiko-User: test-stream" \
+        --header="X-Radiko-Device: pc" \
+        --post-data='\r\n' \
+        --no-check-certificate \
+        --save-headers \
+        --tries=5 \
+        --timeout=5 \
+        -O "auth1_fms_${pid}" \
+        https://radiko.jp/v2/api/auth1_fms
 
     if [ $? -ne 0 ]; then
-	echo "failed auth1 process"
-	return 1
+        echo "auth1 failed"
+        return 1
     fi
 
     #
@@ -83,23 +83,23 @@ auth1() {
 auth2() {
     rm -f "auth2_fms_${pid}"
     wget -q \
-	--header="pragma: no-cache" \
-	--header="X-Radiko-App: pc_1" \
-	--header="X-Radiko-App-Version: 2.0.1" \
-	--header="X-Radiko-User: test-stream" \
-	--header="X-Radiko-Device: pc" \
-	--header="X-Radiko-Authtoken: ${authtoken}" \
-	--header="X-Radiko-Partialkey: ${partialkey}" \
-	--post-data='\r\n' \
-	--no-check-certificate \
-	--tries=5 \
-	--timeout=5 \
-	-O "auth2_fms_${pid}" \
-	https://radiko.jp/v2/api/auth2_fms
+        --header="pragma: no-cache" \
+        --header="X-Radiko-App: pc_1" \
+        --header="X-Radiko-App-Version: 2.0.1" \
+        --header="X-Radiko-User: test-stream" \
+        --header="X-Radiko-Device: pc" \
+        --header="X-Radiko-Authtoken: ${authtoken}" \
+        --header="X-Radiko-Partialkey: ${partialkey}" \
+        --post-data='\r\n' \
+        --no-check-certificate \
+        --tries=5 \
+        --timeout=5 \
+        -O "auth2_fms_${pid}" \
+        https://radiko.jp/v2/api/auth2_fms
 
     if [ $? -ne 0 -o ! -f "auth2_fms_${pid}" ]; then
-	echo "failed auth2 process"
-	return 1
+        echo "auth2 failed"
+        return 1
     fi
 
     echo "authentication success"
@@ -114,65 +114,65 @@ record() {
     #
     # get stream-url
     #
-    if [ -f ${channel}.xml ]; then
-	rm -f ${channel}.xml
+    if [ -f ${station}.xml ]; then
+        rm -f ${station}.xml
     fi
- 
-    wget -q "http://radiko.jp/v2/station/stream/${channel}.xml"
 
-    stream_url=`echo "cat /url/item[1]/text()" | xmllint --shell ${channel}.xml | tail -2 | head -1`
+    wget -q "http://radiko.jp/v2/station/stream/${station}.xml"
+
+    stream_url=`echo "cat /url/item[1]/text()" | xmllint --shell ${station}.xml | tail -2 | head -1`
     url_parts=(`echo ${stream_url} | perl -pe 's!^(.*)://(.*?)/(.*)/(.*?)$/!$1://$2 $3 $4!'`)
- 
-    rm -f ${channel}.xml
+
+    rm -f ${station}.xml
 
     #
     # rtmpdump
     #
     title=`date +"${name} %Y-%m-%d"`
-    basename=`date +"$recordingdir/${dir:+$dir/}${name//\//-}_${channel}_%Y-%m-%d_%H.%M.%S"`
+    basename=`date +"$recordingdir/${dir:+$dir/}${name//\//-}_${station}_%Y-%m-%d_%H.%M.%S"`
     flv="${basename}.flv"
     m4a="${basename}.m4a"
     mkdir -p "$(dirname "$basename")" # basename may contain '/'
 
     rtmpdump -q \
-	-r ${url_parts[0]} \
-	--app ${url_parts[1]} \
-	--playpath ${url_parts[2]} \
-	-W $playerurl \
-	-C S:"" -C S:"" -C S:"" -C S:$authtoken \
-	--live \
-	--stop ${duration} \
-	--flv "$flv"
+        -r ${url_parts[0]} \
+        --app ${url_parts[1]} \
+        --playpath ${url_parts[2]} \
+        -W $playerurl \
+        -C S:"" -C S:"" -C S:"" -C S:$authtoken \
+        --live \
+        --stop ${duration} \
+        --flv "$flv"
 
     if [ $? -eq 1 -o `wc -c "$flv" | awk '{print $1}'` -lt 10240 ]; then
-	return 1
+        return 1
     fi
 
     ffmpeg -y -i "$flv" -vn -acodec copy \
-	-metadata title="$title" \
-	-metadata album="$album" \
-	-metadata artist="$artist" \
-	-metadata genre="Radio" \
-	"$m4a" \
-	&& rm "$flv"
+        -metadata title="$title" \
+        -metadata album="$album" \
+        -metadata artist="$artist" \
+        -metadata genre="Radio" \
+        "$m4a" \
+        && rm "$flv"
 }
 
 
-with_retry() {
+with_retries() {
     cmd=$1
     retries=0
     while :; do
-	$cmd
-	if [ $? -eq 0 ]; then
-	    break
-	elif [ $retries -ge 10 ]; then
-	    echo "failed $cmd"
-	    exit 1
-	else
-	    retries=$(($retries + 1))
-	    sleep $retries
-	    echo "$cmd retry: $retries"
-	fi
+        $cmd
+        if [ $? -eq 0 ]; then
+            break
+        elif [ $retries -ge 10 ]; then
+            echo "$cmd failed after 10 retries"
+            exit 1
+        else
+            retries=$(($retries + 1))
+            sleep $retries
+            echo "Retrying $cmd: $retries"
+        fi
     done
 }
 
@@ -180,20 +180,25 @@ with_retry() {
 # main
 #
 if [ $# -ge 4 ]; then
-  channel=$1
-  duration=$(($2 * 60))
-  name=$3
-  artist=$4
-  dir=$5
-  album="Radio: $name"
+    station=$1
+    duration=$(($2 * 60))
+    name=$3
+    artist=$4
+    dir=$5
+    album="Radio: $name"
 else
-  echo "usage : $0 channel_name duration_minutes name artist [dir_name]"
-  exit 1
+    echo "usage : $0 station_id duration_minutes name artist [subdir]"
+    exit 1
 fi
 
-with_retry get_player
-with_retry get_keydata
-with_retry auth1
-with_retry auth2
-with_retry record
+with_retries get_player
+with_retries get_keydata
+with_retries auth1
+with_retries auth2
+with_retries record
 
+# Local Variables:
+# indent-tabs-mode: nil
+# sh-basic-offset: 4
+# sh-indentation: 4
+# End:
